15acf75e27bd22cb13838c671f01fc8e242c8e4b
fernandopenaranda <fernando.pennaranda@gmail>
2021-12-15 22:05:28 +0100

    Cleaning 2/n

--- src/save.jl
+++ src/save.jl
@@ -1,61 +1,56 @@
 ############################################################################################
 # Saving functions to store the output of figXrun() where X ∈ {2, 3, 4, 5, 6, 7}
 
+using CSV: write, read
+
 "save data"
 function savepsi(figname, p::Params, psi)
-    time_str = string(now())
-    isdir("data") ? nothing : mkdir(data)
-    isdir("data/$(figname)") ? nothing : mkdir("data/$(figname)")
-    pathstr = join([pwd(), "/data/$(figname)/", time_str])
-    mkdir(pathstr) 
-    CSV.write(join([pathstr,"/presets.csv"]), DataFrame(type2dict(p)))
+    path_str = filesstorage_settings()
+    write(join([path_str,"/presets.csv"]), DataFrame(type2dict(p)))
     psi_data = DataFrame(psi = psi)
-    CSV.write(join([pathstr,"/psi.csv"]), psi_data; delim = '\t')
-    return pathstr
+    write(join([path_str,"/psi.csv"]), psi_data; delim = '\t')
+    return path_str
 end
 
 function savespectrum(figname, p::Params, sp1, sp2)
-    time_str = string(now())
-    isdir("data") ? nothing : mkdir("data")
-    isdir("data/$(figname)") ? nothing : mkdir("data/$(figname)")
-    pathstr = join([pwd(), "/data/$(figname)/", time_str])
-    mkdir(pathstr) 
-    CSV.write(join([pathstr,"/presets.csv"]), DataFrame(type2dict(p)))
-    CSV.write(join([pathstr,"/spectrum.csv"]),  DataFrame(sp1, :auto))
+    path_str = filesstorage_settings()
+    write(join([path_str,"/presets.csv"]), DataFrame(type2dict(p)))
+    write(join([path_str,"/spectrum.csv"]),  DataFrame(sp1, :auto))
     psi_data = DataFrame(EZ = sp2)
-    CSV.write(join([pathstr,"/EZ.csv"]), psi_data; delim = '\t')
-    return pathstr
+    write(join([path_str,"/EZ.csv"]), psi_data; delim = '\t')
+    return path_str
 end
 
 "save data"
 function savespectrum(figname, p::Params, sp::Quantica.Spectrum)
-    time_str = string(now())
-    isdir("data") ? nothing : mkdir(data)
-    isdir("data/$(figname)") ? nothing : mkdir("data/$(figname)")
-    pathstr = join([pwd(), "/data/$(figname)/", time_str])
-    mkdir(pathstr) 
-    CSV.write(join([pathstr,"/presets.csv"]), DataFrame(type2dict(p)))
+    path_str = filesstorage_settings()
+    write(join([path_str,"/presets.csv"]), DataFrame(type2dict(p)))
     psi_data = DataFrame(psi = sp.states[:,1])
-    CSV.write(join([pathstr,"/psi.csv"]), psi_data; delim = '\t')
-    return pathstr
+    write(join([path_str,"/psi.csv"]), psi_data; delim = '\t')
+    return path_str
 end
 
 function savecprs(figname, p::Params, phi, ea, eb, ec, ed)
+    path_str = filesstorage_settings()
+    write(join([path_str,"/presets.csv"]), DataFrame(type2dict(p)))
+    phi_data = DataFrame(phi = collect(phi))
+    write(join([path_str,"/phi.csv"]), phi_data; delim = '\t')
+    write(join([path_str,"/cpra.csv"]),  DataFrame(ea, :auto))
+    write(join([path_str,"/cprb.csv"]),  DataFrame(eb, :auto))
+    write(join([path_str,"/cprc.csv"]),  DataFrame(ec, :auto))
+    write(join([path_str,"/cprd.csv"]),  DataFrame(ed, :auto))
+    return path_str
+end
+
+""" check and/or create folder structure"""
+function filesstorage_settings()
     time_str = string(now())
     isdir("data") ? nothing : mkdir("data")
     isdir("data/$(figname)") ? nothing : mkdir("data/$(figname)")
-    pathstr = join([pwd(), "/data/$(figname)/", time_str])
-    mkdir(pathstr)
-    CSV.write(join([pathstr,"/presets.csv"]), DataFrame(type2dict(p)))
-    phi_data = DataFrame(phi = collect(phi))
-    CSV.write(join([pathstr,"/phi.csv"]), phi_data; delim = '\t')
-    CSV.write(join([pathstr,"/cpra.csv"]),  DataFrame(ea, :auto))
-    CSV.write(join([pathstr,"/cprb.csv"]),  DataFrame(eb, :auto))
-    CSV.write(join([pathstr,"/cprc.csv"]),  DataFrame(ec, :auto))
-    CSV.write(join([pathstr,"/cprd.csv"]),  DataFrame(ed, :auto))
-    return pathstr
+    path_str = join([pwd(), "/data/$(figname)/", time_str])
+    mkdir(path_str)
+    return path_str
 end
-
 #fig2
 
 floattostring(vec::Array{Float64,1}) = [floattostring(vec[i]) for i in 1:length(vec)]
@@ -77,50 +72,43 @@ function writecsv(vec, p, name, taglist)
 end
 
 function savecsv(figname, presetsarray::AbstractArray, data::DataFrame) 
-    #check and/or create folder structure
-    time_str = string(now())
-    isdir("data") ? nothing : mkdir(data)
-    isdir("data/$(figname)") ? nothing : mkdir("data/$(figname)")
-    pathstr = join([pwd(), "/data/$(figname)/", time_str])
-    mkdir(pathstr) 
-    #build the csv.files
+    path_str = filesstorage_settings()
     for i in 1:length(presetsarray)
-        CSV.write(join([pathstr,"/presets","$(i)",".csv"]),
+        write(join([path_str,"/presets","$(i)",".csv"]),
             DataFrame(type2dict(presetsarray[i])))
     end
-    CSV.write(join([pathstr,"/$(figname).csv"]), data; delim = '\t')
-    println(join([pathstr,"/$(figname).csv"]))
+    write(join([path_str,"/$(figname).csv"]), data; delim = '\t')
+    println(join([path_str,"/$(figname).csv"]))
 end 
-#
 
 
 #read
 function readfig3data(datapath::String, angle = 0*pi/180)
     println("hey")
-    presets = CSV.read(joinpath(datapath,"presets.csv"), DataFrame,  delim = '\t')
+    presets = read(joinpath(datapath,"presets.csv"), DataFrame,  delim = '\t')
     println(presets)
-    data = CSV.read(joinpath(datapath,"psi.csv"), DataFrame, delim='\t')
+    data = read(joinpath(datapath,"psi.csv"), DataFrame, delim='\t')
     return  map(x->parse(ComplexF64,x), data.psi)
 end
 
 function readfig3psi(datapath::String)
-    presets = CSV.read(joinpath(datapath,"presets.csv"), DataFrame,  delim = '\t')
-    data = CSV.read(joinpath(datapath,"psi.csv"), DataFrame, delim='\t')
+    presets = read(joinpath(datapath,"presets.csv"), DataFrame,  delim = '\t')
+    data = read(joinpath(datapath,"psi.csv"), DataFrame, delim='\t')
     return  data.psi
 end
 
 function readspectrum(datapath::String)
-    y = CSV.read(join([datapath,"/spectrum.csv"]), DataFrame)
-    x = CSV.read(join([datapath,"/EZ.csv"]), DataFrame)
+    y = read(join([datapath,"/spectrum.csv"]), DataFrame)
+    x = read(join([datapath,"/EZ.csv"]), DataFrame)
     return x, y
 end
 
 function readcpr(datapath::String)
-    phi = CSV.read(join([datapath,"/phi.csv"]), DataFrame)
-    cpra = CSV.read(join([datapath,"/cpra.csv"]), DataFrame)
-    cprb = CSV.read(join([datapath,"/cprb.csv"]), DataFrame)
-    cprc = CSV.read(join([datapath,"/cprc.csv"]), DataFrame)
-    cprd = CSV.read(join([datapath,"/cprd.csv"]), DataFrame)
+    phi = read(join([datapath,"/phi.csv"]), DataFrame)
+    cpra = read(join([datapath,"/cpra.csv"]), DataFrame)
+    cprb = read(join([datapath,"/cprb.csv"]), DataFrame)
+    cprc = read(join([datapath,"/cprc.csv"]), DataFrame)
+    cprd = read(join([datapath,"/cprd.csv"]), DataFrame)
     return phi, cpra, cprb, cprc, cprd
 end
 
