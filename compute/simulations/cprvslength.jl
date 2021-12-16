3254287d9debf709e2a9341f0078beff58ce1eeb
fernandopenaranda <fernando.pennaranda@gmail>
2021-12-16 11:51:51 +0100

    Data folder

--- compute/simulations/cprvslength.jl
+++ compute/simulations/cprvslength.jl
@@ -1,11 +1,11 @@
 using MBSinBLG, Quantica, Arpack, Parameters
 using ElectronDisplay
 
-function test(; α = 0, Ey = 4)
+function test(; α = 0, Ey = 4, kw...)
     p = Params(Ls = 0, scale = 40, λ = 5, α = α,   
         μN = 0.6, Δ = 1, d = 0, τ = 1, EZ = SA[0, Ey, 0]);
     lnlist = 50:25:500
-    x, y = cprvsln(lnlist, p, nev = 8)
+    x, y = cprvsln(lnlist, p, nev = 8; kw...)
     return x, y
 end
 
@@ -20,5 +20,22 @@ function cprvsln(list, p; nev = 16, kw...)
 end
 
 
-x_b, y_b = test(α = 0, Ey = 4)
-x_d, y_d = test(α = 1, Ey = 4)
+x_b, y_b = test(α = 0, Ey = 4, W = 1440)
+x_d, y_d = test(α = 1, Ey = 4, W = 1440)
+
+x_b2, y_b2 = test(α = 0, Ey = 4, W = 644)
+x_d2, y_d2 = test(α = 1, Ey = 4, W = 644)
+
+function plottest(x, y,  ylims = (-0.1, 0.1))
+    fig = Figure(resolution = (500, 300), font = "Times New Roman") 
+    axa = Axis(fig[1,1])
+   
+    lines!(axa, x, y[1,:])
+    for i in 2:size(y, 1)
+        lines!(axa, x, y[i,:])
+    end
+    axa.xlabel = " Ln [nm]"
+    axa.ylabel = "E [meV]"
+    ylims!(axa, ylims)
+    fig
+end
--- src/fig7.jl
+++ src/fig7.jl
@@ -4,12 +4,12 @@ function runfig7()
         μN = 0.6, Δ = 1, d = 0, τ = 1, EZ = SA[0, 4, 0]);
     ϕlist = -2π:π/6:2π
     println("1")
-    _, esa = cpr(ϕlist, reconstruct(p, EZ = SA[0, 2, 0], α = 0), nev = 16, W = 1440)
+    _, esa = cpr(ϕlist, reconstruct(p, EZ = SA[0, 2, 0], α = 0), nev = 16, W = 2000)#1440
     println("2")
-    _, esb = cpr(ϕlist, reconstruct(p, EZ = SA[0, 4, 0], α = 0), nev = 16, W = 1440)
+    _, esb = cpr(ϕlist, reconstruct(p, EZ = SA[0, 4, 0], α = 0), nev = 16, W = 2000)
     println("3")
-    _, esc = cpr(ϕlist, reconstruct(p, EZ = SA[0, 2, 0], α = 1), nev = 16, W = 1440)
+    _, esc = cpr(ϕlist, reconstruct(p, EZ = SA[0, 2, 0], α = 1), nev = 16, W = 2000)
     println("4")
-    ϕs, esd = cpr(ϕlist, reconstruct(p, EZ = SA[0, 4, 0], α = 1), nev = 16, W = 1440)
+    ϕs, esd = cpr(ϕlist, reconstruct(p, EZ = SA[0, 4, 0], α = 1), nev = 16, W = 2000)
     savecprs("fig7", p, ϕs, esa, esb, esc, esd)
 end
