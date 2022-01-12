867db8f005abb3f7c6e95b623307ffd26dca89f3
fernandopenaranda <fernando.pennaranda@gmail>
2022-01-12 20:46:03 +0100

    Remove deps

--- src/MBSinBLG.jl
+++ src/MBSinBLG.jl
@@ -2,6 +2,17 @@ module MBSinBLG
     # Use README as the docstring of the module:
     @doc read(joinpath(dirname(@__DIR__), "README.md"), String) MBSinBLG
 
+
+    using Requires
+
+    function __init__()
+        @require CairoMakie = "13f3f980-e62b-5c42-98c6-ff1f3baf88f0" begin
+            @require  VegaLite = "112f6efa-9a02-5b7d-90c0-432ed331239a"
+            @require  Colors = "5ae59095-9a9b-59fe-a467-6f913c188581"
+            @require  LaTeXStrings = "b964fa9f-0449-5b57-a5c2-d3ea65f4040f" include("figplots.jl")
+        end
+    end
+    
     using SharedArrays, Distributed
     using Quantica, StaticArrays, Parameters, LinearAlgebra, StatsBase
     using Baselet, Arpack, StaticArrays, CSV, DataFrames, Dates
@@ -9,8 +20,6 @@ module MBSinBLG
     
     using PhysicalConstants.CODATA2018: ustrip, @u_str, ħ, k_B, m_e, e, μ_B
     
-    using Colors, CairoMakie, ElectronDisplay, LaTeXStrings, VegaLite
-
     export nanoribbonS, nanoribbonSA, nanoribbonSZ, Params, rectangle_weaklink, 
         rectangle_randombounds_sc, splittingvsrotation, savespectrum, modelS,
         spectrumvsphase, savepsi, ldosonlattice_averaged_sc, savespectrumvsphase,
@@ -32,5 +41,4 @@ module MBSinBLG
     include("fig5.jl")
     include("fig6.jl")
     include("fig7.jl")
-    include("figplots.jl")
 end
